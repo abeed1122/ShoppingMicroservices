984c53fa7071e3161c292835b1594abb5ccc08f5
Abeed Uddin <auddin1@partner.jaguarlandrover.com>
2023-07-12 14:24:49 +0100

    Checkin code of Discount.Grpc

--- Services/Discount/Discount.Grpc/Program.cs
+++ Services/Discount/Discount.Grpc/Program.cs
@@ -1,4 +1,5 @@
-using Discount.Grpc.Services;
+using Discount.Grpc.Extensions;
+//using Discount.Grpc.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,9 +10,9 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddGrpc();
 
 var app = builder.Build();
-
+app.MigrateDatabase<Program>();
 // Configure the HTTP request pipeline.
-app.MapGrpcService<GreeterService>();
+//app.MapGrpcService<GreeterService>();
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
 app.Run();
